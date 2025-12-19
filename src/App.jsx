408c123678838ad39515089847606b0aa93db8b0
13-soham <sohammondal821@gmail.com>
2025-12-19 12:06:33 +0530

    initilize-boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <button className='buttonProp m-5'>
+        click
+      </button>
+    </div>
+  )
+}
+
+export default App;
